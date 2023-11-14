e23d4f4e2a6e45e006eddae70293dc3ccb6c79a4
Hai Nguyen <tuj68853@temple.edu>
2023-11-14 14:09:49 -0500

    added code for Retaurant class

--- RestaurantAPI/Controllers/RestaurantController.cs
+++ RestaurantAPI/Controllers/RestaurantController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ClassLibrary1;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Utilities;
 
 namespace RestaurantAPI.Controllers
 {
@@ -7,8 +10,31 @@ namespace RestaurantAPI.Controllers
     //[ApiController]
     public class RestaurantController : Controller
     {
+        [HttpGet]
 
+        public List<Restaurant> Get()
 
+        {
 
+            DBConnect objDB = new DBConnect();
+            DataSet ds = objDB.GetDataSet("SELECT * FROM Restaurants");
+            List<Restaurant> restaurants = new List<Restaurant>();
+
+            Restaurant restaurant;
+
+            foreach (DataRow record in ds.Tables[0].Rows)
+
+            {
+                restaurant = new Restaurant();
+                restaurant.Name = record["Name"].ToString();
+                restaurant.Color = record["Color"].ToString();
+                restaurant.Quantity = record["Quantity"].ToString();
+
+                restaurants.Add(restaurant);
+
+            }
+            return restaurants;
+
+        }
     }
 }
